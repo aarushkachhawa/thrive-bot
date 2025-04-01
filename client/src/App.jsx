12400023faccaa3c0525709906e850b442dc670e
aarushkachhawa <aarushkachhawa@gmail.com>
2025-04-01 22:12:43 +0200

    add to index.html

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './components/LandingPage';
 import SignUpPage from './components/SignUpPage';
 import ConfirmationPage from './components/ConfirmationPage';
-import useGoogleAnalytics from './hooks/useGoogleAnalytics';
 
 function App() {
-  useGoogleAnalytics();
-
   return (
     <Router>
       <div className="App">
--- client/src/components/ConfirmationPage.jsx
+++ client/src/components/ConfirmationPage.jsx
@@ -3,14 +3,15 @@ import { useNavigate } from "react-router-dom";
 
 const ConfirmationPage = () => {
     const navigate = useNavigate();
-  return (
-      <div className="confirmation-page">
-          <h1 className='company-name'>Thrive</h1>
-          <p>You have successfully signed up! We will notify you as soon as Thrive is released.</p>
-
-          <button className="home-button" onClick={() => navigate("/")}>Return Home</button>
-      </div>
-  );
+    return (
+        <div>
+            <div className="confirmation-page">
+                <h1 className='company-name'>Thrive</h1>
+                <p>You have successfully signed up! We will notify you as soon as Thrive is released.</p>
+                <button className="home-button" onClick={() => navigate("/")}>Return Home</button>
+            </div>
+        </div>
+    );
 };
 
 export default ConfirmationPage;
