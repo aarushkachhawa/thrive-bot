cde7fef6785f9e6f6408ff75e2c0f6e6d3a0e8ef
aarushkachhawa <aarushkachhawa@gmail.com>
2025-04-03 11:44:27 +0200

    changed url

--- client/src/components/ChatbotPage.jsx
+++ client/src/components/ChatbotPage.jsx
@@ -5,10 +5,10 @@ import './ChatbotPage.css';
 let API_URL;
 switch (process.env.NODE_ENV) {
     case 'production':
-        API_URL = 'https://thrive-bot-production.up.railway.app';
+        API_URL = 'https://thrive-bot-testing.up.railway.app';
         break;
     case 'development':
-        API_URL = 'https://thrive-bot-testing.up.railway.app';
+        API_URL = 'https://thrive-bot-production.up.railway.app';
         break;
     default:
         API_URL = 'http://localhost:8000';
