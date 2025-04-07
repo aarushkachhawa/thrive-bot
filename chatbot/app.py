233377a4f07dc49ae0babb7edbce8dcd3839b2dc
aarushkachhawa <aarushkachhawa@gmail.com>
2025-04-07 16:45:23 +0200

    added error msg

--- chatbot/app.py
+++ chatbot/app.py
@@ -25,6 +25,9 @@ CORS(app, resources={
 })
 
 openai_api_key = os.getenv('OPENAI_API_KEY')
+if not openai_api_key:
+    raise ValueError("OPENAI_API_KEY environment variable is not set")
+
 client = OpenAI(api_key=openai_api_key)
 
 conversations = {}
